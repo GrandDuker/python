d11ec982da6bb9b09e9186bcffdea0af570eb211
GrandDuker <dcj_ican@163.com>
2018-04-24 16:56:05 +0800

    add new file test

---
+++ arrary.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+ *    > File Name:     arrary.c
+ *    > Company:       ZhuHai ICAN Technology 
+ *    > Author:        D.DUKE
+ *    > Phone-Call:    +86 13249008893
+ *    > Mail:          dcj_ican@163.com 
+ *    > Created Date:  2018年04月24日 星期二 11时58分56秒
+ *************************************************************************/
+
+#include <stdio.h>
+int main(int argc,char *argv[])
+{
+     printf("%c \r\n",argv[1][3]);
+	return 0;
+}
+
---
+++ if-elif.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+if __name__=="__main__":
+     a=30
+     b=20
+     if(a>b):
+          print "a > b"
+     elif(a==b):
+          print "a == b"
+     elif(a<b):
+          print "a < b"
+          i=0
+          for(i=0;i<a;i++):
+               print "hello
